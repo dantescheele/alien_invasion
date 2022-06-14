3d2e5f8b04cad4b2b76cfc6020bd40ec6c00fd1b
Dante <dante_scheele@outlook.com>
2022-06-14 12:22:40 -0400

    Refactored _check_events and _update_screen methods.

--- cat_invasion.py
+++ cat_invasion.py
@@ -24,17 +24,23 @@ class CatInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
+            
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.bg_color)
-            self.puppy.blitme()
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        # Redraw the screen during each pass through the loop.
+        self.screen.fill(self.bg_color)
+        self.puppy.blitme()
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
